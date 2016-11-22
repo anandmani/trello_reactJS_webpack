5f3b7193b5bef4619a6b3e67b6ba5ace71eb8973
Anand Mani <cma.camelot@gmail.com>
2016-11-23 00:58:01 +0530

    Making some comments in index.html regarding bundling

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom'
 // import Draggable from 'react-draggable';
 
 // import Bootstrap from 'bootstrap/dist/css/bootstrap.css';
-require('../../node_modules/bootstrap/dist/css/bootstrap.css');
+// require('../node_modules/bootstrap/dist/css/bootstrap.css');
 import Board from "./components/Board";
 
 ReactDOM.render(<Board/>,document.getElementById("container"));
